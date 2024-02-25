15b3a52bddeb580eca2a69e84223b1278c343190
Grigorii Fedorenko <mr.fursy@gmail.com>
2024-02-25 22:56:57 +0300

    init: start personal-journal

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+import Button from './components/Button/Button';
+
+function App() {
+  return (
+    <>
+      <div>Project</div>
+      <Button />
+    </>
+  );
+}
+
+export default App;
